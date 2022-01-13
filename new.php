24145dee5bdc9fa0f033fec42f431f87d807497c
August Yip <augustyip@gmail.com>
2022-01-13 17:03:27 +0800

    revamp home page

---
+++ new.php
@@ -0,0 +1,354 @@
+<?php
+
+$sections = [
+  [
+    'id' => 'services',
+    'bg' => 'dark',
+    'title' => 'Cloud Architecture Design Service',
+    'desc' => [
+      'August Technologies\' Cloud Architecture Design Service help organizations to transform their on-premise IT infrastructure to cloud services.',
+      'Our cloud experts help the organization define the cloud transformation roadmap and technology blueprint.',
+      'We assess the organization\'s IT infrastructure, applications, security requirements and design the best-fit cloud strategies, architecture, and implementation plan to meeting their business and IT requirements.',
+    ],
+    'list' =>  [
+      [
+        'icon' => 'bi-cloud-check',
+        'title' => 'Landing Zone',
+        'text' => 'The landing zone provides the fundamentals of the cloud building block but still, one of the critical pieces for your cloud strategy. The Landing zone design includes account & tenancy design, identity, and access management (IAM), networking, and security.'
+      ],
+      [
+        'icon' => 'bi-speedometer2',
+        'title' => 'PERFORMANCE AND COST OPTIMIZATION',
+        'text' => 'Performance and Cost optimization brings the heart of the cloud architecture design, unlike the on-premise IT infrastructure, the cost and performance are not always working well together. With the right cloud architecture design, your applications\' performance can be optimized while optimizing your cloud spending. The performance and cost optimization service help the organization design an individual applications\' cloud service sizing, architecture, availability, scale, and service mode based on its requirements and SLA.'
+      ],
+      [
+        'icon' => 'bi-display',
+        'title' => 'MONITORING AND AUTOMATION SERVICE',
+        'text' => 'Monitoring and Automation service helps the organization manage hybrid environments at scale and improve the IT service\'s SLA. Cloud Service brings substantial benefits to the organization but opens up a new challenge to the organization to monitoring and manage multiple and hybrid environments. Our monitoring and automation services help the organization standardize the monitoring framework for their on-premise and cloud resources and streamlining by automation.'
+      ],
+    ],
+  ],
+  [
+    'id' => 'cloud',
+    'bg' => 'light',
+    'title' => 'Cloud Transformation and Migration Service',
+    'desc' => [
+      'The Cloud Transformation provides the possibilities for efficiency gains, increases agility, and create new business models.',
+      'August Technologies Cloud Transformation and Migration service help the organization transform its traditional IT Infrastructure to optimized cloud architecture and advise the best-fit technology, migration strategies, and optimized transformation roadmap for the organization\'s own needs.',
+    ],
+    'list' => [
+      [
+        'icon' => 'bi-card-checklist',
+        'title' => 'CLOUD PLANNING AND DISCOVERY WORKSHOP',
+        'text' => 'August Technologies Cloud specialist works with the customer to understand the business requirements and expected output for the engagement. The workshop allows Cloud specialist planning what technologies fit the organization the most based on their business requirements and what are best migration strategies for the organization based on both business and technology requirements. The workshop mapping out the dependency, destination architecture, and a detailed migration plan.',
+      ],
+      [
+        'icon' => 'bi-clouds',
+        'title' => 'CLOUD MIGRATION SERVICE',
+        'text' => 'August Technologies Migration Service provides various migration strategies for our customers. From lift & ship server migration, application re-platform/modernization, and database re-platform/cloud-native migration. Together with the August Technologies Cloud Planning & Discovery workshop, we are not only helping our customers move their traditional workload to the cloud. More importantly, we help our customers optimize their cloud deployment and cloud strategies.'
+      ],
+      [
+        'icon' => 'bi-cloud-arrow-up',
+        'title' => 'CLOUD DEPLOYMENT SERVICE',
+        'text' => 'The August Technologies Cloud Development Service helps customers to deploy various cloud services from serverless, cloud-native services, containerization, cloud computing, and cloud security and networking solutions. August Technologies Cloud Deployment Services turn the traditional infrastructure into infrastructure as code and help the customers integrate various cloud services and deploy workloads into multiple and hybrid cloud environments.'
+      ],
+    ],
+  ],
+  [
+    'id' => 'managed',
+    'bg' => 'dark',
+    'title' => 'Managed Cloud Service',
+    'desc' => [
+      'The August Technologies Managed Cloud Service helps the organization relieve IT constraints and operational challenges and let the organization focus on what matters for their business.',
+      'The August Technologies Managed Cloud Service adopts industrial best practices, cloud compliance, and automate security incidents response.',
+      'The August Technologies Managed Cloud Service helps organizations streamline operational efficiency, improve business resilience, cloud security, and cloud compliance.',
+    ],
+    'list' => [
+      [
+        'icon' => 'bi-server',
+        'title' => 'MANAGED CLOUD ENVIRONMENT',
+        'text' => 'Our cloud expert manages multiple cloud environments, operate, and support various cloud services for our customers. Through innovation and technologies, August Technologies\' managed cloud service provides a high service level and short response time. Since no one managed cloud service that will fit all the cloud customers, during the onboarding process, our Cloud Professional will map out the end to end process and tailor the managed service for our customers.',
+      ],
+      [
+        'icon' => 'bi-shield-check',
+        'title' => 'MANAGED CLOUD SECURITY',
+        'text' => 'Our cloud-managed engineers continuously and proactively monitor the infrastructure and applications. Our SecOps team helps our customers build SecOps practice and automate important security tasks to ensures that organizations do not compromise on security when achieving uptime and performance goals. Our managed cloud security service work together with IT operation and IT security team seamlessly and provide security monitoring, security automation, configuration management incident management.'
+      ],
+    ],
+  ],
+  [
+    'id' => 'iot',
+    'bg' => 'light',
+    'title' => 'Big Data & IoT',
+    'desc' => [
+      'August Technologies Big Data & IoT consultancy help customers design & architect enterprise data platforms and build sophisticated data platforms in the clouds and customers\' premise.',
+      'Our Big Data & IoT engineers help customers turn historical, data warehouse, and real-time data into the data-driven solution. Our innovative and outcome-based solution design helps the customers quickly adopt new technologies and transform raw data into valuable insights and monetize their big data use cases.',
+    ],
+    'list' => [
+      [
+        'icon' => 'bi-graph-up-arrow',
+        'title' => 'BIG DATA CONSULTING',
+        'text' => 'Design High-Level Architecture of big data solution based on the organization\'s business requirements and the business\' use cases. Tailor the big data solution blueprint and roadmap based on what matters to most to the organization. Our Big Data engineers help the customers define data flows, data pipeline, and end-to-end solution components. Enable the organization to re-realize the hidden value of its data.',
+      ],
+      [
+        'icon' => 'bi-diagram-3',
+        'title' => 'IOT SOLUTION',
+        'text' => 'Our IoT data integration engineers build and integrate various sensors and IoT platforms for our customers. We develop IoT solutions on all the major cloud and on-premise platforms. We provide sensors solution in different infrastructure and senors protocols such as NB-IoT, LoRa, Sigfox, 4G/3G, and Wifi network and sensors hardware from Video sensors, 3D Lidars, Finger Vein and various Smart City senors for building and environmental solution.'
+      ],
+    ],
+  ],
+];
+
+$features = [
+  [
+    'icon' => 'bi-lightning-charge-fill',
+    'title' => 'Quick time to market gives you an edge',
+    'text' => 'Whether you need to manage cost, infrastructure capacity, and agility of resources or simply improve latency and performance issues, we offer a wide-range of cloud solutions to best fit your business.'
+  ],
+  [
+    'icon' => 'bi-people-fill',
+    'title' => 'Build the right team to scale',
+    'text' => 'Finding the right talent is not easy. We help you find the talent that suits your needs, follows your processes, and sticks with you long term. Our delivery model helps you cut costs and deliver within budget.'
+  ],
+  [
+    'icon' => 'bi-code-slash',
+    'title' => 'Code &amp; Architecture',
+    'text' => 'Cloud &amp; Software architecture are defined as designing parts that are hard to change later. We aimed at the long-term health of a product (e.g., code refactoring, feature rewrites, or, data migration) and to avoid technical debt.'
+  ],
+];
+
+
+$links = [
+  [
+    'icon' => 'bi-facebook',
+    'href' => '#',
+  ],
+  [
+    'icon' => 'bi-linkedin',
+    'href' => '#',
+  ],
+  [
+    'icon' => 'bi-twitter',
+    'href' => '#',
+  ],
+  [
+    'icon' => 'bi-github',
+    'href' => '#',
+  ],
+];
+
+$contacts = [
+  [
+    'icon' => 'bi-pin-map',
+    'href' => '#',
+    'text' => 'Room 1905 Nan Fung Centre, 264-298 Castle Peak Road, Tsuen Wan, N.T., Hong Kong',
+  ],
+  [
+    'icon' => 'bi-at',
+    'href' => 'mailto:august@august.hk',
+    'text' => 'august@august.hk',
+  ],
+  [
+    'icon' => 'bi-telephone',
+    'href' => 'tel:+85294946701',
+    'text' => '+852 9494 6701',
+  ],
+];
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+  <title>Hello, world!</title>
+</head>
+
+<body class="text-white bg-dark">
+  <!-- Header Area wrapper Starts -->
+  <header id="header-wrap">
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-dark navbar-light sticky-top">
+      <div class="container">
+        <a class="navbar-brand text-white " href="#">Site Title</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <!-- <span class="navbar-toggler-icon"></span> -->
+          <i class="bi bi-justify text-white display-6"></i>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#feature">Features</a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown link
+              </a>
+              <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item text-white" href="#">Action</a></li>
+                <li><a class="dropdown-item text-white" href="#">Another
+                    action</a></li>
+                <li><a class="dropdown-item text-white" href="#">Something else
+                    here</a></li>
+              </ul>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Pricing</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Navbar End -->
+
+    <!-- Hero Area Start -->
+    <div class="bg-dark text-secondary px-4 py-5 mt-5 text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12">
+            <div class="contents text-center">
+              <h2 class="display-5 fw-bold text-white">We Discover, Design &amp;
+                Build
+                Digital <br> Presence of Businesses</h2>
+              <div class="">
+                <a href="#services" class="btn btn-outline-light my-2 px-4 rounded-pill">Cloud
+                  Architecture
+                  and Design</a>
+                <a href="#cloud" class="btn btn-outline-light my-2 px-4 rounded-pill">Cloud
+                  Transformation and Migration</a>
+                <a href="#managed" class="btn btn-outline-light my-2 px-4 rounded-pill">Managed
+                  Cloud</a>
+                <a href="#iot" class="btn btn-outline-light my-2 px-4 rounded-pill">Big Data
+                  &amp; IoT</a>
+              </div>
+            </div>
+            <!-- <div class="img-thumb text-center wow fadeInUp" data-wow-delay="0.6s">
+                    <img class="img-fluid" src="assets/img/hero-1.png" alt="">
+                  </div> -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Hero Area End -->
+
+  </header>
+  <!-- Header Area wrapper End -->
+  <!-- Feature Section Start -->
+  <section id="feature" class="">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-6 col-md-12 col-sm-12 bg-light text-dark">
+          <div class="text-wrapper p-4 mt-4 ms-4 ps-4">
+            <h2 class="display-6">Solutions for Your Business</h2>
+            <p class="mb-4 pt-4">Whether you need to manage cost, infrastructure
+              capacity, and agility of resources or simply improve latency and
+              performance issues, we offer a wide-range of cloud solutions to
+              best fit your business.</p>
+            <!-- <a href="#" class="btn btn-common">More About Us</a> -->
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 padding-none bg-secondary">
+
+        <?php
+        foreach($features as $f) :
+        ?>
+          <div class="card mb-3 bg-secondary border-0">
+            <div class="row g-0">
+              <div class="col-md-2 p-3 text-center ">
+                <!-- <img src="/assets/img/lightning-charge-fill.svg" alt="lightning-charge" class="img-fluid rounded-start" width="100"> -->
+                <i class="bi <?=$f['icon'];?> text-white display-2"></i>
+              </div>
+              <div class="col-md-10">
+                <div class="card-body">
+                  <h5 class="card-title"><?=$f['title'];?></h5>
+                  <p class="card-text"><?=$f['text'];?></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        <?php endforeach ?>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Feature Section End -->
+
+  <?php foreach($sections as $s) : ?>
+  <section id="<?=$s['id'];?>" class="container-fluid p-3 <?=($s['bg'] == 'dark') ? '' : 'bg-light text-dark';?>">
+    <div class="container">
+      <div class="text-justify">
+        <h4 class="text-center mb-4"><?=$s['title'];?></h4>
+        <?php foreach($s['desc'] as $p): ?>
+          <p class="text-justify"><?=$p;?></p>
+        <?php endforeach;?>
+      </div>
+
+      <div class="list-group px-5">
+      <?php foreach ($s['list'] as $l) :?>
+        <div class="list-group-item mb-3 p-4  <?=($s['bg'] == 'dark') ? '' : 'bg-dark text-white';?>">
+          <h5 class="mb-3 text-uppercase display-6 "><i class="bi <?=$l['icon'];?> me-3 text-center"></i><?=$l['title'];?></h5>
+          <p class="mb-1"><?=$l['text'];?></p>
+        </div>
+      <?php endforeach;?>
+      </div>
+    </div>
+  </section>
+  <?php endforeach;?>
+
+  <section id="contact" class="container-fluid">
+    <div class="row">
+      <div class="col bg-white text-dark p-3">
+        <h5 class="ps-3">We're a friendly bunch..</h5>
+        <p  class="text-justify text-secondary ps-3">We create projects for companies and startups with a passion for quality</p>
+      </div>
+      <div class="col p-3">
+        <h5 class="ps-3">CONTACT US</h5>
+        <div class="list-group">
+        <?php foreach($contacts as $c): ?>
+          <a href="<?=$c['href'];?>" class="list-group-item bg-dark text-white d-flex align-items-center" ><i class="bi <?=$c['icon'];?> me-3 fs-4"></i><?=$c['text'];?></a>
+        <?php endforeach;?>
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+  <footer class="container py-2">
+    <div class="row justify-content-end">
+      <div class="col-auto">
+        <div class="list-group list-group-horizontal">
+        <?php foreach($links as $l): ?>
+          <a href="<?=$l['href'];?>"  class="list-group-item bg-dark text-white"><i class="bi <?=$l['icon'];?>"></i></a>
+        <?php endforeach;?>
+        </div>
+      </div>
+      <div class="col-auto align-self-center">
+        © <?=date('Y');?> August Technologies Limited. All Rights Reserved.
+      </div>
+    </div>
+  </footer>
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>

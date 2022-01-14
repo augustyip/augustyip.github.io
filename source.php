a557e93eadcd0fb2b8cae983ba6e4bf98d8ac017
August Yip <augustyip@gmail.com>
2022-01-14 10:33:43 +0800

    add back to top button

--- source.php
+++ source.php
@@ -344,7 +344,7 @@ $contacts = [
   </footer>
 
   <!-- Back to top button -->
-<button type="button" class="btn btn-danger btn-lg position-fixed bottom-0 end-0  " id="btn-back-to-top">
+<button type="button" class="btn btn-warning btn-lg position-fixed" style="bottom: 10px; right: 10px; display: none;" id="btn-back-to-top">
   <i class="bi bi-arrow-up-circle"></i>
 </button>
   <!-- Optional JavaScript; choose one of the two! -->
@@ -357,6 +357,34 @@ $contacts = [
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-</body>
+
+    <script>
+      //Get the button
+      let mybutton = document.getElementById("btn-back-to-top");
+
+      // When the user scrolls down 20px from the top of the document, show the button
+      window.onscroll = function () {
+        scrollFunction();
+      };
+
+      function scrollFunction() {
+        if (
+          document.body.scrollTop > 20 ||
+          document.documentElement.scrollTop > 20
+        ) {
+          mybutton.style.display = "block";
+        } else {
+          mybutton.style.display = "none";
+        }
+      }
+      // When the user clicks on the button, scroll to the top of the document
+      mybutton.addEventListener("click", backToTop);
+
+      function backToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+  </body>
 
 </html>
